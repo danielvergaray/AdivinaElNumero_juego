1aa42d1d1d29edc4324d7db4f9b93c5c2b27f9c8
Daniel Vergaray <danielvergarayescobar@gmail.com>
2023-08-10 16:09:18 -0500

    Se ha culminado la funcionalidad del juego, falta mejorar el codigo

--- js/codigo.js
+++ js/codigo.js
@@ -1,38 +1,155 @@
 // Relacionado al numero secreto
-let numeroAzar= Math.trunc(Math.random()*20)+1;
-let signo= document.getElementById("signoInterrogacion");
-signo.innerText=numeroAzar;
+let numeroAzar = Math.trunc(Math.random() * 20) + 1;
+let signo = document.getElementById("signoInterrogacion");
+/* signo.innerText = numeroAzar; */
 
 // Score inicial
-let score=20;
-document.getElementById("score").textContent=score;
+let score = 20;
+document.getElementById("score").textContent = score;
+let highscore = 0;
+document.getElementById("highscore").textContent = highscore;
+let playing = true;
+
+mensajeAlUsuario.innerText = "Comienza a adivinar";
 
 //Llamo al evento
-let btnCheck= document.getElementById("btn-check");
-btnCheck.addEventListener("click",  verificar );
+let btnCheck = document.getElementById("btn-check");
+btnCheck.addEventListener("click", verificar);
 
 
-function verificar (){
-    
-    document.getElementById("score").textContent=score;
-    let highscore=0;
-    let numeroUsuario=Number(document.getElementById("inputNumero").value);
-   
-    if (numeroUsuario===numeroAzar){
-        let acertaste= '🎊 Felicidades, acertaste!';
-        mensajeAlUsuario.innerText=acertaste;
+function verificar() {
 
-    } else if (numeroUsuario<numeroAzar){
-        let mensaje= '📉 Estas por debajo';
-        mensajeAlUsuario.innerText=mensaje;
-        score--;
-        document.getElementById("score").textContent=score;  
-    } else if (numeroUsuario>numeroAzar){
-        let mensaje= '📉 Estas por arriba';
-        mensajeAlUsuario.innerText=mensaje;
-        score--;
-        document.getElementById("score").textContent=score;    
+    document.getElementById("score").textContent = score;
+
+    let numeroUsuario = Number(document.getElementById("inputNumero").value);
+
+    if (playing && numeroUsuario) {
+        if (numeroUsuario >= 1 && numeroUsuario <= 20) {
+            if (numeroUsuario === numeroAzar) {
+                let acertaste = '🎊 Felicidades, acertaste!';
+                mensajeAlUsuario.innerText = acertaste;
+                /* highscore = score; */
+                document.getElementById("highscore").textContent = highscore;
+                let fondoGanaste = document.getElementById("fondoGanaste");
+                fondoGanaste.classList.add("ganaste");
+                signo.innerText = numeroAzar;
+                hasGanado()
+
+                if (highscore < score) {
+                    highscore = score;
+                    document.getElementById("highscore").textContent = highscore;
+                }
+
+                playing = false;
+
+
+            } else if (numeroUsuario !== numeroAzar) {
+                if (numeroUsuario < numeroAzar) {
+                    let mensaje = ' 📉 Estas por debajo';
+                    mensajeAlUsuario.innerText = mensaje;
+                    score--;
+                    document.getElementById("score").textContent = score;
+                    alertaPorDebajo();
+                } else {
+                    let mensaje = ' 📉 Estas por arriba';
+                    mensajeAlUsuario.innerText = mensaje;
+                    score--;
+                    document.getElementById("score").textContent = score;
+                    alertaPorArriba();
+                }
+
+
+            }
+        } else {
+            mensajeAlUsuario.innerText = "INGRESAR UN NUMERO ENTRE 1 Y 20";
+        }
+
+    }
+    else if (playing && numeroUsuario === 0) {
+        mensajeAlUsuario.innerText = "INGRESAR UN NUMERO ENTRE 1 Y 20";
     }
 
-    /* console.log(numeroUsuario.value) */
+    if (score === 0) {
+        mensajeAlUsuario.innerText = "Has perdido ☹ - Intentalo nuevamente";
+        fondoGanaste.classList.add("perdiste");
+        hasPerdido();
+        playing = false;
+    }
+
+
+
+
+
 }
+
+let repetir = document.getElementById("repetirJuego");
+repetir.addEventListener("click", function () {
+    score = 20;
+    fondoGanaste.classList.remove("ganaste");
+    fondoGanaste.classList.remove("perdiste");
+    numeroAzar = Math.trunc(Math.random() * 20) + 1;
+    signo.innerText = "?";
+    document.getElementById("inputNumero").value = "";
+    mensajeAlUsuario.innerText = "Comienza a adivinar";
+    document.getElementById("score").textContent = score;
+    playing = true
+
+    /* if(highscore<score){
+        highscore= score;
+    } */
+
+
+})
+
+function alertaPorDebajo() {
+
+    Toastify({
+        text: "📉 Estas por debajo",
+        className: "info",
+        duration: 1000,
+        style: {
+            background: "black",
+        }
+    }).showToast();
+}
+function alertaPorArriba() {
+
+    Toastify({
+        text: "📉 Estas por arriba",
+        className: "info",
+        duration: 1000,
+        style: {
+            background: "black",
+        }
+    }).showToast();
+}
+
+function hasGanado() {
+
+    Swal.fire({
+        position: 'top-center',
+        title: 'HAS GANADO',
+        imageUrl: './homero-feliz.png',
+        imageHeight: 500,
+        imageAlt: 'A tall image',
+        showConfirmButton: false,
+        timer: 2000
+    })
+}
+function hasPerdido() {
+
+    Swal.fire({
+        position: 'top-center',
+        title: 'HAS PERDIDO',
+        imageUrl: './homero-triste.png',
+        imageHeight: 500,
+        imageAlt: 'A tall image',
+        showConfirmButton: false,
+        timer: 2000
+    })
+}
+
+
+
+
+
